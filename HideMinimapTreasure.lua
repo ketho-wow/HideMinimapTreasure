a52224cee9e88f9423e936995bdf4edda6cff2ce
Ketho <sghs17@outlook.com>
2020-11-25 15:55:15 +0100

    Initial commit

---
+++ HideMinimapTreasure.lua
@@ -0,0 +1,4 @@
+-- source: https://github.com/Gethe/wow-ui-textures/blob/beta/MINIMAP/ObjectIconsAtlas.PNG
+-- exported to TGA with GIMP
+
+Minimap:SetBlipTexture("Interface/AddOns/HideMinimapTreasure/objecticonsatlas_novignetteloot")
